6ddddb013cdd6c3429d0155b9b66fca2d8064f1b
Nir Bar <nir.bar@panel-sw.co.il>
2019-05-01 10:37:18 +0300

    Support property types: Int, String, Bool; default is String.

--- MSBuild.CSharp.DefineConstants/DefineConstantsTask.cs
+++ MSBuild.CSharp.DefineConstants/DefineConstantsTask.cs
@@ -3,6 +3,7 @@ using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text.RegularExpressions;
@@ -22,9 +23,15 @@ namespace MSBuild.CSharp.DefineConstants
             try
             {
                 string targetPath = TargetAssembly.ItemSpec;
+                if (!Path.IsPathRooted(targetPath))
+                {
+                    targetPath = Path.Combine(Environment.CurrentDirectory, targetPath);
+                }
+
                 string targetDir = Path.GetDirectoryName(targetPath);
                 string targetFile = Path.GetFileName(targetPath);
                 string targetFileBase = Path.GetFileNameWithoutExtension(targetPath);
+                Log.LogMessage(MessageImportance.High, $"Creating constants assembly '{targetPath}'");
 
                 AssemblyName aName = new AssemblyName(targetFileBase);
                 AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(aName, AssemblyBuilderAccess.RunAndSave, targetDir);
@@ -40,7 +47,6 @@ namespace MSBuild.CSharp.DefineConstants
 
                 if (!Log.HasLoggedErrors)
                 {
-                    Log.LogMessage(MessageImportance.High, $"Creating constants assembly '{targetPath}'");
                     Type t = tb.CreateType();
                     ab.Save(targetFile);
                 }
@@ -50,6 +56,13 @@ namespace MSBuild.CSharp.DefineConstants
                 Log.LogErrorFromException(ex, true);
             }
             return !Log.HasLoggedErrors;
+        }
+
+        private enum PropertyType
+        {
+            String,
+            Bool,
+            Int
         }
 
         private readonly Regex identifierRegex_ = new Regex("^[a-z_][a-z0-9_]*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -83,6 +96,34 @@ namespace MSBuild.CSharp.DefineConstants
                 return;
             }
 
+            // Property type
+            PropertyType pt = PropertyType.String;
+            string typeStr = item.GetMetadata("Type");
+            if (!string.IsNullOrWhiteSpace(typeStr) && !Enum.TryParse(typeStr, out pt))
+            {
+                Log.LogWarning($"Property '{key}' has unsupported type '{typeStr}'. Supported types are: {Enum.GetNames(typeof(PropertyType)).Aggregate((all, a) => all + ',' + a)}");
+                pt = PropertyType.String;
+            }
+
+            Type type = typeof(string);
+            switch (pt)
+            {
+                case PropertyType.Bool:
+                    type = typeof(bool);
+                    break;
+
+                case PropertyType.Int:
+                    type = typeof(int);
+                    break;
+
+                case PropertyType.String:
+                default:
+                    pt = PropertyType.String;
+                    type = typeof(string);
+                    break;
+            }
+
+
             // Warn on duplicate
             if (properties_.ContainsKey(key))
             {
@@ -93,11 +134,36 @@ namespace MSBuild.CSharp.DefineConstants
                 return;
             }
 
-            PropertyBuilder pb = typeBuilder.DefineProperty(key, PropertyAttributes.None, typeof(string), null);
+            PropertyBuilder pb = typeBuilder.DefineProperty(key, PropertyAttributes.None, type, null);
             MethodAttributes getAttr = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig | MethodAttributes.Static;
-            MethodBuilder mbGetAccessor = typeBuilder.DefineMethod($"get_{key}", getAttr, typeof(string), Type.EmptyTypes);
+            MethodBuilder mbGetAccessor = typeBuilder.DefineMethod($"get_{key}", getAttr, type, Type.EmptyTypes);
             ILGenerator getIL = mbGetAccessor.GetILGenerator();
-            getIL.Emit(OpCodes.Ldstr, val);
+            switch (pt)
+            {
+                case PropertyType.Bool:
+                    bool b;
+                    if (!bool.TryParse(val, out b))
+                    {
+                        Log.LogError($"Can't use '{val}' as a boolean value as specified by 'Type' metadata for item '{key}'");
+                        return;
+                    }
+                    getIL.Emit(b ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+                    break;
+
+                case PropertyType.Int:
+                    int i;
+                    if (!int.TryParse(val, out i))
+                    {
+                        Log.LogError($"Can't use '{val}' as an integer value as specified by 'Type' metadata for item '{key}'");
+                        return;
+                    }
+                    getIL.Emit(OpCodes.Ldc_I4, i);
+                    break;
+
+                case PropertyType.String:
+                    getIL.Emit(OpCodes.Ldstr, val);
+                    break;
+            }
             getIL.Emit(OpCodes.Ret);
             pb.SetGetMethod(mbGetAccessor);
             Log.LogMessage(MessageImportance.Low, $"Created constant property '{key}'='{val}'");
